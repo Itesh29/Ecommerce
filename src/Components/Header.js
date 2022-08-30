244d2fa2e0dd38c1f8149e51459598a42e249d20
Itesh <iteshnani@gmail.com>
2022-08-30 23:40:30 +0530

    firstcommit

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import {  BiSearch } from "react-icons/bi";
+import { MdBusinessCenter,MdOutlineShoppingBasket } from "react-icons/md";
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <div className='header-logo'>
+                <MdBusinessCenter className='title-logo'/>
+                <h2 className='title'> eShop </h2>
+            </div>
+            <div className='header-search'>
+                <input className='search' type='text'></input>
+                <BiSearch className='search-logo'/>
+            </div>
+            <div className='header-nav'>
+                <div className='nav-item'>
+                    <span className='line-one'> Hello USer </span>
+                    <span className='line-two'> Sign-Up</span>
+                </div>
+                <div className='nav-item'>
+                    <span className='line-one'> Your </span>
+                    <span className='line-two'> Shop </span>
+                </div>
+                <div className='nav-item'>
+                    <MdOutlineShoppingBasket className='cart-logo'/>
+                    <span className='line-two'> 0 </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
